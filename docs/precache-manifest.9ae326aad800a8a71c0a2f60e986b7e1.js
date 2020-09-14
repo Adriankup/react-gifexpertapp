77c07a678240d4f14d7b0d8a02da8db9a3f6c3e6
Adrian Ku <kupechcesar@gmail.com>
2020-09-14 14:30:46 -0500

    docs agregado

---
+++ docs/precache-manifest.9ae326aad800a8a71c0a2f60e986b7e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04400151876beb53b173e4d0b24fba45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e19431d768ac6105297d",
+    "url": "/static/css/main.d00bcfff.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e19431d768ac6105297d",
+    "url": "/static/js/main.297d8c99.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
